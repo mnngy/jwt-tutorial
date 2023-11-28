e7330b94f2c623606b078787eb22b8c31aea7103
민인규 <ingyu1218@flow.team>
2023-11-28 23:02:42 +0900

    WebSecurity 설정 추가

--- src/main/java/com/example/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/example/jwttutorial/config/SecurityConfig.java
@@ -4,11 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring().requestMatchers("/h2-console/**", "/favicon.ico");
+        };
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
